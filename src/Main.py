823305cf5b6d850989da08f009576240820cce36
Lukasz Kuk <lukasz0104201@wp.pl>
2020-10-09 18:10:58 +0200

    updated generation path

--- src/Main.py
+++ src/Main.py
@@ -24,22 +24,24 @@ scripts_path = env_path + 'Scripts\\'
 input_data_path = input_path + 'data\\'
 output_data_path = output_path + 'data\\'
 output_tmp_path = output_path + 'tmp\\'
+output_tmp2_path = output_path + 'tmp2\\'
 output_tiles_path = output_path + 'tiles\\'
 
 input_25k = input_data_path + '25k\\'
+input_25kTmp = input_data_path + '25kTmp\\'
 input_250k = input_data_path + '250k\\'
 
 output_merged_path = output_path + 'merged\\'
-merged_file = output_merged_path + 'merged.tiff'
-warped_file = output_merged_path + 'warped.tiff'
-translated_file = output_merged_path + 'translated.tiff'
+merged_file = output_merged_path + 'merged.tif'
+warped_file = output_merged_path + 'warped.tif'
+translated_file = output_merged_path + 'translated.tif'
 
 input_icc = input_path + 'OS_Map_uncoated_FOGRA29_GCR_bas.icc'
 output_icc = input_path + 'sRGB_v4_ICC_preference.icc'
 
 
-def getFilesList(directory):
-    return glob.glob(directory + '*.tif')
+def getFilesList(directory, file_ext='*.tif'):
+    return glob.glob(directory + file_ext)
 
 
 def getFilesString(directory):
@@ -47,17 +49,17 @@ def getFilesString(directory):
     return " ".join(files)
 
 
-def copyFiles(src, dest, regex='*.t*', delete_content=True):
+def copyFiles(src, dest, file_name_regex='*.t*', delete_dest_before_copy=True):
     print("Copying files...")
-    if delete_content:
-        deleteDirectoryWithContent(dest)
+    if delete_dest_before_copy:
+        deleteDirectory(dest)
     makeDirectory(dest)
-    for file_path in glob.glob(os.path.join(src, '**', regex), recursive=True):
+    for file_path in glob.glob(os.path.join(src, '**', file_name_regex), recursive=True):
         new_path = os.path.join(dest, os.path.basename(file_path))
         shutil.copy(file_path, new_path)
 
 
-def deleteDirectoryWithContent(path):
+def deleteDirectory(path):
     try:
         shutil.rmtree(path, ignore_errors=True)
     except FileNotFoundError:
@@ -78,70 +80,106 @@ def makeDirectory(path):
         pass
 
 
-def profileToProfile(data_path, out_path):
+def profileToProfile(input_data, out_path):
     print("Profiling...")
     makeDirectory(out_path)
-    files = getFilesList(data_path)
+    files = getFilesList(input_data)
     for image in files:
         in_image = Image.open(image)
 
-        out_im = ImageCms.profileToProfile(in_image, inputProfile=input_icc, outputProfile=output_icc, outputMode='RGB')
+        out_im = ImageCms.profileToProfile(in_image, inputProfile=input_icc, outputProfile=output_icc,
+                                           outputMode='RGB', renderingIntent=ImageCms.INTENT_ABSOLUTE_COLORIMETRIC)
         file_name = image.split('\\')
         file_name_len = len(file_name)
         file_name = file_name[file_name_len - 1]
         out_im.save(out_path + file_name)
 
 
-def gdalMerge(data_path):
+def translateIntoOneFile(input_data, out_path):
+    deleteDirectory(out_path)
+    makeDirectory(out_path)
+    files = getFilesList(input_data)
+    for file in files:
+        file_name = file.split('\\')
+        file_name_len = len(file_name)
+        file_name = file_name[file_name_len - 1]
+        params = [
+            '-of', 'GTiff',
+            '-ot', 'Byte',
+            '-b', '1',
+            '-b', '2',
+            '-b', '3',
+            # '-expand', 'rgb',
+            # '-co', 'PHOTOMETRIC=CMYK',
+            '-co', 'COMPRESS=DEFLATE',
+            file,
+            out_path + file_name
+        ]
+        subprocess.call([env_path + 'Library\\bin\\gdal_translate.exe'] + params)
+
+
+def gdalMerge(input_data, out_file, is_pct=False):
     print("Merging...")
-    deleteDirectoryWithContent(output_merged_path)
+    deleteDirectory(output_merged_path)
     makeDirectory(output_merged_path)
-    files_to_merge = getFilesList(data_path)
-    params = ['-pct',
-              '-of', 'GTiff',
-              # '-co', 'ALPHA=NO',
-              '-co', 'TFW=YES',
-              '-co', 'COMPRESS=DEFLATE',
-              '-o', merged_file]
+    files_to_merge = getFilesList(input_data)
+    pct = []
+    if is_pct:
+        pct = ['-pct']
+    params = pct + [
+        '-of', 'GTiff',
+        '-ot', 'Byte',
+        # '-co', 'ALPHA=NO',
+        # '-co', 'PHOTOMETRIC=RGB',
+        '-co', 'COMPRESS=DEFLATE',
+        # '-co', 'TILED=YES',
+        '-o', out_file]
     params = params + files_to_merge
     subprocess.call(["python", scripts_path + "gdal_merge.py"] + params)
 
 
-def gdalWarp():
-    print("Warping...")
-    deleteFile(warped_file)
-    params = ['-s_srs', 'EPSG:27700',
-              '-t_srs', 'EPSG:3857',
-              # '-nosrcalpha',
-              '-of', 'GTiff',
-              '-co', 'PHOTOMETRIC=RGB',
-              '-co', 'COMPRESS=DEFLATE',
-              translated_file,
-              warped_file]
-    subprocess.call([env_path + 'Library\\bin\\gdalwarp.exe'] + params)
-
-
-def gdalTranslate():
+def gdalTranslate(input_file, out_file):
     print("Translating...")
-    deleteFile(translated_file)
-    params = ['-of', 'GTiff',
-              '-expand', 'rgb',
-              '-co', 'PHOTOMETRIC=RGB',
-              '-co', 'COMPRESS=DEFLATE',
-              merged_file,
-              translated_file]
+    deleteFile(out_file)
+    params = [
+        '-of', 'GTiff',
+        '-ot', 'Byte',
+        '-b', '1',
+        '-b', '2',
+        '-b', '3',
+        # '-expand', 'rgb',
+        # '-co', 'PHOTOMETRIC=RGB',
+        '-co', 'COMPRESS=DEFLATE',
+        # '-co', 'TILED=YES',
+        input_file,
+        out_file]
     subprocess.call([env_path + 'Library\\bin\\gdal_translate.exe'] + params)
 
 
-def gdal2Tiles(zoom):
+def gdalWarp(in_file, out_file):
+    print("Warping...")
+    deleteFile(out_file)
+    params = ['-s_srs', 'EPSG:27700',
+              '-t_srs', 'EPSG:3857',
+              '-of', 'GTiff',
+              # '-co', 'PHOTOMETRIC=RGB',
+              '-co', 'COMPRESS=DEFLATE',
+              # '-co', 'TILED=YES',
+              in_file,
+              out_file]
+    subprocess.call([env_path + 'Library\\bin\\gdalwarp.exe'] + params)
+
+
+def gdal2Tiles(in_file, zoom):
     print("Tiling...")
-    deleteDirectoryWithContent(output_tiles_path)
+    deleteDirectory(output_tiles_path)
     makeDirectory(output_tiles_path)
-    params = ['-s', 'EPSG:3857',
-              '--xyz',
-              '-z', zoom,
-              warped_file,
-              output_tiles_path]
+    params = [
+        '-s', 'EPSG:3857',
+        '--xyz',
+        '-z', zoom,
+        in_file,
+        output_tiles_path]
     subprocess.call(["python", scripts_path + 'gdal2tiles.py'] + params)
 
 
@@ -174,22 +212,27 @@ def main(argv=None):
     for opt, arg in opts:
         if opt == '-k':
             use_profile = True
-            input_dir = input_25k
+            input_dir = input_25kTmp
         if opt == '-z':
             zoom = arg
 
+    warp_in_file = merged_file
     if use_profile:
-        # copyFiles(input_dir, output_data_path)
-        copyFiles(input_dir, output_tmp_path)
-        profileToProfile(output_tmp_path, output_data_path)
-        copyFiles(output_tmp_path, output_data_path, "*.TFW", False)
-        deleteDirectoryWithContent(output_tmp_path)
+        # copyFiles(src=input_dir, dest=output_tmp_path)
+        # profileToProfile(input_data=output_tmp_path, out_path=output_tmp2_path)
+        # copyFiles(src=output_tmp_path, dest=output_tmp2_path, file_name_regex="*.TFW", delete_dest_before_copy=False)
+        translateIntoOneFile(input_data=output_tmp2_path, out_path=output_data_path)
+        # deleteDirectory(path=output_tmp_path)
+        # deleteDirectory(path=output_tmp2_path)
+        gdalMerge(input_data=output_data_path, out_file=merged_file)
+        gdalTranslate(input_file=merged_file, out_file=translated_file)
     else:
-        copyFiles(input_dir, output_data_path)
-    gdalMerge(output_data_path)
-    gdalTranslate()
-    gdalWarp()
-    gdal2Tiles(zoom)
+        copyFiles(src=input_dir, dest=output_data_path)
+        gdalMerge(input_data=output_data_path, out_file=merged_file, is_pct=True)
+        gdalTranslate(input_file=merged_file, out_file=translated_file)
+        warp_in_file = translated_file
+    # gdalWarp(in_file=warp_in_file, out_file=warped_file)
+    # gdal2Tiles(in_file=warped_file, zoom=zoom)
 
 
 def main2(argv=None):
@@ -203,7 +246,7 @@ def main2(argv=None):
     img_path = '11\\' + '987\\' + '622.png'
     # img_path = '13\\' + '3902\\' + '2476.png'
 
-    img1_path = input_data_path + img_path
+    img1_path = input_path + img_path
     img2_path = output_tiles_path + img_path
     img1 = Image.open(img1_path).convert('RGB')
     img2 = Image.open(img2_path).convert('RGB')
